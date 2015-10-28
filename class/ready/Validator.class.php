e5b1683e95ff05fc8b95ec877604cec8ff2486c2
Zzepish <zzepish13@mail.ru>
2015-10-28 23:33:38 +0300

    Все еще редактирую валидатор

--- class/ready/Validator.class.php
+++ class/ready/Validator.class.php
@@ -2,7 +2,10 @@
 class Validator{
 		
 		private $validationArray; 
-		public static $errorArray=[];
+		private static $errorArray=[
+			'arrayCounterMissmatched' => 'Не верное число входящих данных в массиве!',
+			'name' => 'Вы не ввели имя, или ввели его не корректно! Имя не может содержать меньше 2-х символов!'
+		];
 	   
 		public function __construct(){
 
@@ -20,26 +23,30 @@ class Validator{
 								];
 		}
 		
+		private static function validationResult($booleanResult, $error){
+			return [$booleanResult, $error];
+		}
+		
 		public function arrayValidator($data, $compareArrayType = null){
 
 			if(count($data)!= count($this->validationArray[$compareArrayType]))
-				return false;
+				return self::validationResult(false, self::$errorArray['arrayCounterMissmatched']);
 				
 			foreach($data as $key => $value)
 				if(
 					!isset($this->validationArray[$compareArrayType]) || 
-					!$this->validationArray[$compareArrayType][$key]($value)
+					!($result=$this->validationArray[$compareArrayType][$key]($value))[0]
 				)
-					return false;
+					return $result;
 						
-				return true;
+				return self::validationResult(true,'Валидация типа "'.$compareArrayType.'" выполнена успешно');
 		}
 			
 		public static function validateName($data){
-			return is_string($data);
+			return self::validationResult(is_string($data), self::$errorArray[$data]);
 		}
 		
 		public static function validatePhone($data){
-			return is_string($data) and preg_match('#^\+[0-9]{2}\([0-9]{3}\)[0-9]{7}$#',$data);
+			return self::validationResult((is_string($data) and preg_match('#^\+[0-9]{2}\([0-9]{3}\)[0-9]{7}$#',$data)), self::$errorArray[$data]);
 		}
 	}
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			$validator = new Validator();
 			$time = microtime(1);
 			for($i = 0 ; $i < 100000 ; $i++ )
-				$validator->arrayValidator($data,'register')? 'Valid' : 'Un-valid';
+				echo ($result = $validator->arrayValidator($data,'register')[0]) ? 'Valid' : $result[1];
 			
 			echo '<br> Time:'.(microtime(1)-$time);
 			echo '<br> Memory usage: '.memory_get_usage()/1024/1024 .' Mb'; 
